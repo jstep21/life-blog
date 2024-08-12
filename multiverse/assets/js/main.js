6809260c71db81e3a729bb3b8fcc9ee9c7a0ea86
jstep21 <jstep21@github.com>
2024-08-12 15:18:04 +0200

    added side dropdown menu for skills and photos

--- multiverse/assets/js/main.js
+++ multiverse/assets/js/main.js
@@ -8,6 +8,7 @@
 
 	var	$window = $(window),
 		$body = $('body'),
+		$menu = $('#menu'),
 		$wrapper = $('#wrapper');
 
 	// Breakpoints.
@@ -18,6 +19,58 @@
 			small:   [ '481px',   '736px'  ],
 			xsmall:  [ null,      '480px'  ]
 		});
+
+	// Menu.
+		$menu
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Search (header).
+		var $search = $('#search'),
+			$search_input = $search.find('input');
+
+		$body
+			.on('click', '[href="#search"]', function(event) {
+
+				event.preventDefault();
+
+				// Not visible?
+					if (!$search.hasClass('visible')) {
+
+						// Reset form.
+							$search[0].reset();
+
+						// Show.
+							$search.addClass('visible');
+
+						// Focus input.
+							$search_input.focus();
+
+					}
+
+			});
+
+		$search_input
+			.on('keydown', function(event) {
+
+				if (event.keyCode == 27)
+					$search_input.blur();
+
+			})
+			.on('blur', function() {
+				window.setTimeout(function() {
+					$search.removeClass('visible');
+				}, 100);
+			});
 
 	// Hack: Enable IE workarounds.
 		if (browser.name == 'ie')
